75b8d2ab7dc2eab8537c03be00068df93e7080f8
djumanov <diyorbekjumanov1120@gmail.com>
2022-12-12 19:58:14 +0500

    done

---
+++ square.py
@@ -0,0 +1,45 @@
+class Square:
+    def __init__(self, square_side:float):
+        """
+        is_valid() -> bool
+        area() -> float
+        perimeter() -> float
+        """
+        pass
+
+    def is_valid(self) -> bool:
+        """
+        This method checks if the circle is valid.
+
+        Args:
+            No
+        Returns:
+            bool: This method checks if the square is valid.
+        """
+        pass
+
+    
+    def area(self):
+        
+        """
+        This method finds the area of the square.
+
+        Args:
+            No
+        Returns:
+            float or int: return area of the square if the square is valid, 0 otherwise
+        """
+        pass
+    
+
+    def perimeter(self):
+
+        """
+        This method finds the perimeter of the square.
+
+        Args:
+            No
+        Returns:
+            float: return perimeter of the square if the square is valid, 0 otherwise
+        """
+        pass
---
+++ triangle.py
@@ -0,0 +1,50 @@
+from math import sqrt
+
+class Triangle:
+    def __init__(self, a:float, b:float, c:float):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def is_valid(self) -> bool:
+        '''
+        This method checks if the triangle is valid.
+        
+        Args: 
+            No
+        Returns:
+            bool: True if the triangle is valid, False otherwise
+        '''
+        pass
+
+    
+    def get_type(self) -> str:
+        '''
+        This method finds the type of the triangle.
+        ''' 
+        pass
+
+
+    def perimeter(self):
+    
+        '''
+        This method finds the perimeter of the triangle.
+
+        Args:
+            No
+        Returns:
+            int or float: return perimeter of the triangle if the triangle is valid, 0 otherwise
+        '''
+        pass
+
+
+    def area(self):
+        '''
+        This method finds the area of the triangle.
+
+        Args:
+            NO
+        Returns:
+            int or float: return area of the triangle if the triangle is valid, 0 otherwise
+        '''
+        pass
